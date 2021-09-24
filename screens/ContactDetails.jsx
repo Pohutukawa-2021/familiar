0837e8d6afd4cd21219df4d3f5a56f0924b59497
austin <austin.weng@yahoo.co.nz>
2021-09-24 16:44:42 +1200

    add style to contactDetails component

--- screens/ContactDetails.jsx
+++ screens/ContactDetails.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import { StyleSheet, Button, View, Text, Alert } from 'react-native'
+import { StyleSheet, Button, View, Text, Alert, Pressable } from 'react-native'
 
 function ContactDetails (props) {
   const { name, number, frequency, daysSinceLastCall } = props
   return (
 
     <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={styles.h1}>Dad</Text>
+      </View>
       <View style={styles.innerContainer} >
         <Text style={styles.label}>Name: {name}</Text>
         <Text style={styles.label}>Number:{number}</Text>
         <Text style={styles.label}>Frequency: {frequency}</Text>
         <Text style={styles.label}>Days since last call:{daysSinceLastCall}</Text>
-        <View>
-          <Button title={`Call ${name}`}/>
-          <Button title='Edit' {...props} onPress={() => props.navigation.navigate('Edit')}/>
+        <View style={styles.buttonView}>
+          <Pressable style={styles.button}/*  onPress={handlePress} */>
+            <Text style={styles.buttonText}>{`Call ${name}`}</Text>
+          </Pressable>
+        </View>
+        <View style={styles.buttonView}>
+          <Pressable style={styles.button} onPress={() => props.navigation.navigate('Edit')}>
+            <Text style={styles.buttonText}>Edit</Text>
+          </Pressable>
         </View>
       </View>
     </View>
@@ -25,26 +34,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center'
+  },
+  topContainer: {
+    width: '100%',
+    height: '35%',
+    backgroundColor: 'red',
+    justifyContent: 'center'
+
   },
   innerContainer: {
     width: '80%',
     marginTop: 40,
-    margin: 10
+    padding: 20
   },
   h1: {
-    fontSize: 50,
+    fontSize: 80,
+    color: 'white',
     fontWeight: 'bold',
-    marginBottom: 30
+    marginBottom: 30,
+    alignSelf: 'center'
   },
   label: {
     width: '80%',
-    fontSize: 25
+    fontSize: 20,
+    marginBottom: 5
+  },
+  buttonView: {
+    width: '100%',
+    marginTop: 40,
+    borderRadius: 35
   },
   button: {
-    width: '100%',
-    marginTop: 40
+    backgroundColor: '#5AF160',
+    padding: 10,
+    borderRadius: 35
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    alignSelf: 'center'
   }
 })
 
