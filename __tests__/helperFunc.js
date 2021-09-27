50eef1ddc90cd745c571b336b8adfec48eaaefae
csiyang <csiyang@yahoo.co.nz>
2021-09-27 13:53:31 +1300

    set up test layout

---
+++ __tests__/helperFunc.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { readData, saveData, color } from '../helpers/helperFunc'
+
+describe('All functions should return correct values', () => {
+  it('color returns the correct color', () => {
+
+  })
+
+  it.skip('saveData should update the async storage', () => {
+
+  })
+
+  it.skip('readData should obtain all the data in the correct shape', () => {
+
+  })
+})
