8063e27a6429246b667243c1fec1573f1c8d5884
Tadhg <tadhg@pop-os.localdomain>
2021-09-28 10:17:57 +1300

    fix test error

--- __tests__/Home.js
+++ __tests__/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { fireEvent } from '@testing-library/react-native'
+import { act, cleanup } from '@testing-library/react-native'
 import Home from '../screens/Home'
 import { renderWithNavigation } from '../jest/test-utils'
 
@@ -9,14 +9,16 @@ afterAll(() => {
   jest.resetAllMocks()
 })
 
+afterEach(cleanup)
+
 // when localStorage is empty home dsplays welcome message
 // buttons redirect appropriatly
 // right amount of cards
 
-it('home screen displays message when no contacts have been added', () => {
+it('home screen displays message when no contacts have been added', async () => {
   const mockNavigate = jest.fn()
 
   const { getByText } = renderWithNavigation(<Home navigation={{ navigate: mockNavigate }}/>, 'stack')
 
-  expect(getByText(/Press/)).toBeTruthy()
+  await act(async () => { expect(getByText(/Press/)).toBeTruthy() })
 })
