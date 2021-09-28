28215cecc84e731c8bd7a8cfb594de7108e8e65e
AliakbarSu <aliakbar.su@hotmail.com>
2021-09-28 15:34:40 +1300

    Add history page

--- screens/History.js
+++ screens/History.js
@@ -1,12 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 /* eslint-disable-next-line */
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { useIsFocused } from '@react-navigation/native'
+import { readData } from '../helpers/helperFunc'
+import moment from 'moment'
 
-function History (props) {
+function History(props) {
+  const [data, setData] = useState([])
+  const isFocused = useIsFocused()
+
+  useEffect(() => {
+    async function getData() {
+      const data = await readData()
+      if (data) {
+        setData(data)
+      } else {
+        setData([])
+      }
+    }
+    getData()
+  }, [isFocused])
+
   return (
     <View style={styles.container}>
-      <Text>History calllog</Text>
-      {/* {props.callLog.map()} */}
+      <View style={styles.textBox}>
+        <Text style={styles.label}>Call History</Text>
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.innerContainer}
+      >
+        {data.length > 0 ? (
+          <View style={styles.listContainer}>
+            {data.map((contact, index) => {
+              const difference = moment().diff(contact.lastCall, 'days')
+
+              return (
+                <View
+                  key={contact.name}
+                  style={[
+                    styles.listItemContainer,
+                    index % 2 === 0
+                      ? styles.greyBackground
+                      : styles.whiteBackground
+                  ]}
+                >
+                  <View style={styles.listItem}>
+                    <View style={styles.textContainer}>
+                      <Text>{contact.name}</Text>
+                    </View>
+                    <View style={styles.textContainer}>
+                      <Text>{contact.callCount}</Text>
+                    </View>
+                  </View>
+                </View>
+              )
+            })}
+          </View>
+        ) : (
+          <Text style={styles.emptyText}>Press + to add some contacts!</Text>
+        )}
+      </ScrollView>
+      {/* <Button title='Set' onPress={handleSet} />
+    <Button title='Clear' onPress={handleClear} /> */}
     </View>
   )
 }
@@ -15,8 +72,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  innerContainer: {
+    width: '90%',
+    marginTop: 20,
+    marginBottom: 60
+  },
+  listContainer: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%'
+  },
+  listItemContainer: {
+    width: '100%',
+    padding: 18
+  },
+  listItem: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  textContainer: {
+    flex: 1
+  },
+  greyBackground: {
+    backgroundColor: '#E8E7E7'
+  },
+  whiteBackground: {
+    backgroundColor: '#F9F9F9'
+  },
+  textBox: {
+    marginTop: 0,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderColor: 'darkgrey',
+    backgroundColor: '#22CAFF'
+  },
+  label: {
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: 40,
+    marginBottom: 10
   }
 })
 
